fec33d93625f1f8578afba5ed702ee1c0bc6d64a
MythicalPlayz <eyaddodo05@gmail.com>
2025-02-25 00:32:30 +0200

    rewriting the local multiplayer p1

--- scripts/multi_ui.gd
+++ scripts/multi_ui.gd
@@ -1,9 +1,11 @@
 extends Control
 
-@export var address = "127.0.0.1" #LOCALHOST
+# export address, peer
+@export var address = "127.0.0.1"
 @export var port = 18042
 var peer
 
+# ready function
 func _ready() -> void:
 	multiplayer.peer_connected.connect(peerConnected)
 	multiplayer.peer_disconnected.connect(peerDisonnected)
@@ -11,95 +13,157 @@ func _ready() -> void:
 	multiplayer.connection_failed.connect(connectionFailed)
 
 
-#called on server/client	
+# peer Connected (runs on clients/server)
 func peerConnected(id):
-	print('Player has connected: ' + str(id))
+	# print('Player has connected: ' + str(id))
+	pass
 	
+# peer Disconnected (runs on clients/server)	
 func peerDisonnected(id):
-	print('Player has disonnected: ' + str(id))
+	# print('Player has disonnected: ' + str(id))
 	
+	# if players in game then kick them out
 	if GameManager.ingame:
 		GameManager.returnhome()
 	
+	# if host has left
 	if id == 1:
-		# Host has left
+		
+		# reset buttons and players
 		GameManager.players.clear()
-		$LineEdit.editable = true
+		%PlayerNameLabel.editable = true
 		ipedit('',true)
 		changebuttonsstate(true)
 	else:
+		
+		# only remove the player
 		GameManager.players.erase(id);
-	getPlayerNames.rpc()
-	changePlayerCount()
+		
+	# change player count	
+	getPlayerNames()
 	
-# called only client	
+# connected Server (client)
 func connectedServer():
-	print('Connected to Server')
-	#sendPlayerInfo.rpc_id(1,$LineEdit.text,multiplayer.get_unique_id())
-	sendPlayerInfo.rpc($LineEdit.text,multiplayer.get_unique_id())
+	#print('Connected to Server')
+	
+	# Disable UI
+	send_playerinfo_toserver.rpc(%PlayerNameLabel.text,multiplayer.get_unique_id())
 	changebuttonsstate(false)
 	statushandle('Connection Success!')
 	
+# connection Failed (client)
 func connectionFailed():
 	print('Connection Fail')
 	changebuttonsstate(true)
 	statushandle('Connection Failed!')
 	
-	
+# host button pressed	
 func _on_host_button_button_down() -> void:
 	
+	# get local ip
 	var ip = get_local_ip()
 	ipedit(ip,false)
+	
+	# create new peer
 	peer = ENetMultiplayerPeer.new()
 	var error = peer.create_server(port,4)
 	
+	# check for errors
 	if error != OK:
 		print('Error Could not Host')
 		statushandle('Hosting Error: Could not Host!')
 		return
 	
+	# set peer and compression
 	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 	multiplayer.set_multiplayer_peer(peer)
-	sendPlayerInfo($LineEdit.text,1)
-	$LineEdit.editable = false
-	$PanelContainer/HBoxContainer/StartButton.disabled = false
-	changePlayerCount()
-	print('Waiting for players')
+	
+	# send player info
+	send_playerinfo_toserver(%PlayerNameLabel.text,1)
+	
+	# prevent UI
+	%PlayerNameLabel.editable = false
+	#%StartButton.disabled = false
+	
+	# change states
+	getPlayerNames()
+	#print('Waiting for players')
 	statushandle('Hosting Success!')
 	changebuttonsstate(false)
 	
-@rpc('any_peer')
-func sendPlayerInfo(name, id):
-	#var start_time = Time.get_ticks_usec()
-	# Check if player does not already exist
-	if not GameManager.players.has(id):
-		GameManager.players[id] = {
-			'name': name,
-			'id': id,
+# send player info
+#### REMEMBER
+## Server → Client: @rpc("authority", "call_remote", "reliable")
+## Server → Every peer: @rpc("authority", "call_local", "reliable")
+## Client → Server: @rpc("any_peer", "call_remote", "reliable")
+## Authority Client → Server: @rpc("authority", "call_remote", "reliable") set_multiplayer_authority(peer_id)
+
+# send player info to from client to server
+@rpc("any_peer", "call_remote", "reliable")
+func send_playerinfo_toserver(player_name, player_mid):
+
+	# check if player does not already exist
+	if not GameManager.players.has(player_mid):
+		GameManager.players[player_mid] = {
+			'name': player_name,
+			'id': player_mid,
 			'score': 0
 		 }
-	# If the server, broadcast the player info
-	if multiplayer.is_server():
-	#changePlayerCount()
-		for player_id in GameManager.players:
-			sendPlayerInfo.rpc(GameManager.players[player_id].name, player_id)
-			getPlayerNames.rpc()
-	#var end_time = Time.get_ticks_usec()
-	#print("Process time: ", end_time - start_time, " µs")
+	
+	# broadcast the player info to clients
+	for player_id in GameManager.players:
+		
+		# prevent server
+		if player_id == 1: continue
+		
+		# send player info to other clients
+		send_playerinfo_toclient.rpc_id(player_id,GameManager.players)
+	
+	# get the player names for UI
+	getPlayerNames()
+		
+# send player info to from server to clients
+@rpc("any_peer", "call_remote", "reliable")
+func send_playerinfo_toclient(server_players):
 
-@rpc("any_peer","call_local")
+	for player_id in server_players:
+		# check if player does not already exist
+		if not GameManager.players.has(player_id):
+			GameManager.players[player_id] = {
+				'name': server_players[player_id].name,
+				'id': player_id,
+				'score': 0
+		 	}
+	
+	# get the player names for UI
+	getPlayerNames()
+# function get player names on UI
 func getPlayerNames():
-	if $Players/VBoxContainer:
-		var node = $Players/VBoxContainer
-		for n in node.get_children():
-			node.remove_child(n)
-			n.queue_free()
-		changePlayerCount()
-		for player_id in GameManager.players:
-			var label = Label.new()
-			label.name = str(player_id) + '_ID'
-			label.text = GameManager.players[player_id].name
-			node.add_child(label)
+	# get container
+	var players_container = %PlayersPanel.get_child(0)
+	
+	# loop through text if player has left
+	for label in players_container.get_children():
+		if GameManager.players.get(str(label.name)): continue
+		players_container.remove_child(label)
+		label.queue_free()
+	
+	# loop through players
+	for player_id in GameManager.players:
+		var player_name = GameManager.players[player_id].name
+		
+		# check if text is there, skip
+		var exists = players_container.get_node_or_null(player_name+'_ID')
+		if exists: continue
+		
+		# create label
+		var label = Label.new()
+		label.name = str(player_id) + '_ID'
+		label.text = player_name
+		players_container.add_child(label)
+		
+	# change player count
+	changePlayerCount()
 
 func _on_join_button_button_down() -> void:
 	var address = $IPLabel.text
@@ -108,7 +172,7 @@ func _on_join_button_button_down() -> void:
 	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
 	ipedit(address,false)
 	multiplayer.set_multiplayer_peer(peer)	
-	$LineEdit.editable = false
+	%PlayerNameLabel.editable = false
 
 
 func _on_start_button_button_down() -> void:
@@ -126,17 +190,16 @@ func startGame():
 	var scene = load("res://game/main.tscn").instantiate()
 	get_tree().root.add_child(scene)
 	self.hide()
-	sendPlayerInfo($LineEdit.text,multiplayer.get_unique_id())
 	print('START')
 	GameManager.world = scene
 	GameManager.mui = self
 	GameManager.ingame = true
 	GameManager.game_cycle()
 	
-	
+# set player count in the UI	
 func changePlayerCount():
 	var c = len(GameManager.players)
-	$PlayerCount.text = 'Player Count (' + str(c) + '/4)'
+	%PlayerCountLabel.text = 'Player Count (' + str(c) + '/4)'
 	
 	
 @rpc("call_local",'any_peer')
@@ -145,12 +208,11 @@ func sendUpdate(message):
 
 func changebuttonsstate(state):
 	state = not state
-	$PanelContainer/HBoxContainer/HostButton.disabled = state
-	$PanelContainer/HBoxContainer/JoinButton.disabled = state
+	%HostButton.disabled = state
+	%JoinButton.disabled = state
 	
 func _on_line_edit_text_changed(new_text: String) -> void:
-	var text = $LineEdit.text
-	if text:
+	if new_text:
 		changebuttonsstate(true)
 	else:
 		changebuttonsstate(false)
